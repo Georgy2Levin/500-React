cf134030ba4d5cccba76d207433cdbd017c5fc3b
Georgy Levin <g.phyzy@gmail.com>
2024-12-29 09:28:46 +0200

    common gallery w/o dividing by places

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ function App() {
             <Route path='/' element={<Layout placesKeysPlacesNames={placesKeysPlacesNames} />}>
               <Route index element={<Home fotos={fotos} />} />
               {getRoutes()}
-              <Route path='all' element={<GalleriesPage placesFotos={placesNamesFotos} />} />
+              <Route path='all' element={<GalleryPage placeName='Moscow' fotos={fotos} />} />              
             </Route>
           </Routes>
         </BrowserRouter>
